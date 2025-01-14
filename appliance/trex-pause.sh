d6720a3cc70fdf461c0adb7e473eb7d80589b242
johnny-raicu <johnny.raicu@gmail.com>
2025-01-14 14:40:51 -0600

    updated script

--- appliance/trex-pause.sh
+++ appliance/trex-pause.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-curl -s "http://127.0.0.1:4067/control?sid=`curl -s 'http://127.0.0.1:4067/login?password=@Ionel400'|cut -f4 -d'"'`&pause=true"
+PASSWD="TBD"
+curl -s "http://127.0.0.1:4067/control?sid=`curl -s 'http://127.0.0.1:4067/login?password=${PASSWD}'|cut -f4 -d'"'`&pause=true"
 echo "t-rex paused"
--- appliance/trex-start.sh
+++ appliance/trex-start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-curl -s "http://127.0.0.1:4067/control?sid=`curl -s 'http://127.0.0.1:4067/login?password=@Ionel400'|cut -f4 -d'"'`&pause=false"
+PASSWD="TBD"
+curl -s "http://127.0.0.1:4067/control?sid=`curl -s 'http://127.0.0.1:4067/login?password=${PASSWD}'|cut -f4 -d'"'`&pause=false"
 echo "t-rex paused"
